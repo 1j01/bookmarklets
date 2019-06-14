1df1daed9418912db0ca4e5e7ac5f3409db3b482
Isaiah Odhner <isaiahodhner@gmail.com>
2019-06-14 13:03:51 -0400

    Refactor for granular edit testing

--- mutagen.js
+++ mutagen.js
@@ -1,38 +1,75 @@
-function mutateCode(code, mutation_chance) {
-	// console.log("mutateCode with mutation_chance=", mutation_chance);
-	// TODO: remove \[ when adding compile-fail checking
-	// TODO: replace negative_match_group with negative lookahead if possible
-	var new_code = code.replace(/([^a-z$_\[]|^)(0x[\dA-F]+|\d+(?:\.\d*)?|\.\d+)(?!e)/gi, (match_str, negative_match_group_str, num_str)=> {
-		// console.log(`matched ${num_str} (full match_str = ${match_str})`);
-		if (isNaN(num_str)) { console.log(`somehow matched ${num_str}`); }
-		var n = parseFloat(num_str);
-		var original_n = n;
-		// important: keep it as float or int (determined by whether there's a decimal point in GLSL)
-		var had_dot = num_str.indexOf(".") > -1;
-		var cast = (n)=> {
-			if (had_dot && `${n}`.indexOf(".") === -1) {
-				return `${n}.0`;
-			} else if (!had_dot) {
-				return `${Math.ceil(n)}`;
-			}
-			return n;
-		}
-		console.log(`matched ${num_str}`, {n, had_dot, num_str});
+function findEditPoints(code, mutation_chance) {
+	var doc = [];
+	var editPoints = new Set();
 
-		// console.log("mutate number", n);
-		if (Math.random() < mutation_chance) { n += 1; }
-		if (Math.random() < mutation_chance) { n -= 1; }
-		if (Math.random() < mutation_chance) { n /= 2; }
-		if (Math.random() < mutation_chance) { n *= 2; }
-		if (isNaN(n)) {
-			console.log(`somehow got ${n} from ${JSON.stringify(num_str)}`);
-			return negative_match_group_str + num_str;
-		} else if (n < 0 && original_n >= 0) {
-			return negative_match_group_str + "(" + cast(n - 1) + ")";
+	// the first group here is in lieu of negative lookbehinds in javascript
+	var numbersRegExp = /([^a-z$_]|^)(0x[\dA-F]+|\d+(?:\.\d*)?|\.\d+)(?!e)/gi;
+	var match;
+	var lastMatchLastIndex = 0;
+	while ((match = numbersRegExp.exec(code)) !== null) {
+		var [match_str, negative_match_group_str, num_str] = match;
+		console.log('Found', {match, match_str, negative_match_group_str, num_str});
+		console.log('Next match starts at ' + numbersRegExp.lastIndex);
+		doc.push(code.slice(lastMatchLastIndex, match.index + negative_match_group_str.length));
+		if (isNaN(num_str)) {
+			console.log(`somehow matched ${num_str} (full match_str = ${match_str})`);
+			doc.push(editPoint);
+		} else {
+			var editPoint = {
+				type: "number",
+				originalStr: num_str,
+				modifiedStr: mutateNumber(num_str, mutation_chance), // TODO: do this in separate step, remove mutation_chance from args above
+			};
+			editPoints.add(editPoint);
+			doc.push(editPoint);
 		}
-		return negative_match_group_str + cast(n);
-	});
-	return new_code;
+		lastMatchLastIndex = numbersRegExp.lastIndex;
+	}
+	doc.push(code.slice(lastMatchLastIndex));
+
+	return {doc, editPoints};
+}
+
+function mutateNumber(num_str, mutation_chance) {
+	var n = parseFloat(num_str);
+	var original_n = n;
+
+	// keep number as float or int (determined by whether there's a decimal point in GLSL)
+	var had_dot = num_str.indexOf(".") > -1;
+	var cast = (n)=> {
+		if (had_dot && `${n}`.indexOf(".") === -1) {
+			return `${n}.0`;
+		} else if (!had_dot) {
+			return `${Math.ceil(n)}`;
+		}
+		return n;
+	}
+	// console.log(`matched ${num_str}`, {n, had_dot, num_str});
+
+	// console.log("mutate number", n);
+	if (Math.random() < mutation_chance) { n += 1; }
+	if (Math.random() < mutation_chance) { n -= 1; }
+	if (Math.random() < mutation_chance) { n /= 2; }
+	if (Math.random() < mutation_chance) { n *= 2; }
+	if (isNaN(n)) {
+		console.warn(`somehow got ${n} from ${JSON.stringify(num_str)}`);
+		return num_str;
+	} else if (n < 0 && original_n >= 0) {
+		return "(" + cast(n - 1) + ")";
+	}
+	return cast(n);
+}
+
+function renderDocToString(doc, editPointsSet) {
+	return doc.map((fragment)=> {
+		if (typeof fragment === "string") {
+			return fragment;
+		} else if (editPointsSet.has(fragment)) {
+			return fragment.modifiedStr;
+		} else {
+			return fragment.originalStr;
+		}
+	}).join("");
 }
 
 function getCodeFromPage() {
@@ -51,9 +88,12 @@ function setCodeOnPage(new_code) {
 	var cm = document.querySelector('.CodeMirror').CodeMirror;
 	cm.setValue(new_code);
 }
-function findCompileFailure() {
+function didCompileFail() {
 	// TODO: bytebeat
-	return document.querySelector(".tab.errorYes, .CodeMirror .errorMessage");
+	if (document.querySelector(".tab.errorYes, .CodeMirror .errorMessage")) {
+		return true;
+	}
+	return false;
 }
 function isCompiling() {
 	// TODO: bytebeat
@@ -68,7 +108,7 @@ function compileCodeOnPage() {
 			if (isCompiling()) {
 				setTimeout(waitForCompileFinish, 50);
 			} else {
-				var success = !findCompileFailure();
+				var success = !didCompileFail();
 				if (success) resolve(); else reject(new Error("compile failed"));
 			}
 		}
@@ -80,8 +120,12 @@ function mutateCodeOnPage() {
 	var original_code = getCodeFromPage();
 	var mutation_chance = 0.05 + 1 / original_code.length;
 
-	var new_code = mutateCode(original_code, mutation_chance);
-	// console.log("new_code", new_code);
+	// TODO: try sets of edits in a sort of binary search of whether it can compile with a given set (accept set of edits) and whether a particular single edit makes it not compile (reject one edit)
+
+	var {doc, editPoints} = findEditPoints(original_code, mutation_chance);
+	console.log({doc, editPoints});
+	var new_code = renderDocToString(doc, editPoints);
+	console.log("new_code", new_code);
 	if (new_code === original_code) {
 		console.log("new_code is same as original_code, LAME");
 	}
@@ -114,8 +158,8 @@ addButtonToUI();
 mutateCodeOnPage();
 
 /*
-TODO: try sets of edits in a sort of binary search of whether it can compile with a given set (accept set of edits) and whether a particular single edit makes it not compile (reject one edit)
 some other things that would be good:
+detect not just compilation failure but also blank canvas (all pixels same color)
 support bytebeat again [on windows93.net too]
 khan academy, including "error buddy" detection
 code fiddles like jsfiddle, codepen, jsbin
