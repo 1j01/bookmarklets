d3463e5732991e294a981a843b044e11498e58c6
Isaiah Odhner <isaiahodhner@gmail.com>
2019-06-14 11:21:47 -0400

    Update for ShaderToy and extensibility

--- mutagen.js
+++ mutagen.js
@@ -1,24 +1,126 @@
-var button = document.getElementById("mutate") || document.createElement("button");
-button.id = "mutate";
-button.textContent = "☢ MUTATE ☢";
-document.querySelector("#controls").appendChild(button);
-button.onclick = ()=> {
-	var textarea = document.querySelector("#code");
-	var mutation_chance = 0.05 + 1 / textarea.value.length;
+function mutateCode(code, mutation_chance) {
+	// console.log("mutateCode with mutation_chance=", mutation_chance);
+	// TODO: remove \[ when adding compile-fail checking
+	// TODO: replace negative_match_group with negative lookahead if possible
+	var new_code = code.replace(/([^a-z$_\[]|^)(0x[\dA-F]+|\d+(?:\.\d*)?|\.\d+)(?!e)/gi, (match_str, negative_match_group_str, num_str)=> {
+		// console.log(`matched ${num_str} (full match_str = ${match_str})`);
+		if (isNaN(num_str)) { console.log(`somehow matched ${num_str}`); }
+		var n = parseFloat(num_str);
+		var original_n = n;
+		// important: keep it as float or int (determined by whether there's a decimal point in GLSL)
+		var had_dot = num_str.indexOf(".") > -1;
+		var cast = (n)=> {
+			if (had_dot && `${n}`.indexOf(".") === -1) {
+				return `${n}.0`;
+			} else if (!had_dot) {
+				return `${Math.ceil(n)}`;
+			}
+			return n;
+		}
+		console.log(`matched ${num_str}`, {n, had_dot, num_str});
 
-	var new_code = textarea.value.replace(/([^a-z$_]|^)(0x[\\dA-F]+|\\d+(?:\\.\\d*)?|\\.\\d+)(?!e)/gi, (matchStr, negativeMatchBit, numStr)=> {
-		var n = parseFloat(numStr);
+		// console.log("mutate number", n);
 		if (Math.random() < mutation_chance) { n += 1; }
-		if (Math.random() < mutation_chance) { if(n - 1 < 0 && n >= 0) { return negativeMatchBit + "(" + (n - 1) + ")"; } n -= 1; }
+		if (Math.random() < mutation_chance) { n -= 1; }
 		if (Math.random() < mutation_chance) { n /= 2; }
 		if (Math.random() < mutation_chance) { n *= 2; }
-		if (isNaN(n)) { console.log("somehow got "+n+" from "+JSON.stringify(numStr)); n = numStr; }
-		return negativeMatchBit + n;
+		if (isNaN(n)) {
+			console.log(`somehow got ${n} from ${JSON.stringify(num_str)}`);
+			return negative_match_group_str + num_str;
+		} else if (n < 0 && original_n >= 0) {
+			return negative_match_group_str + "(" + cast(n - 1) + ")";
+		}
+		return negative_match_group_str + cast(n);
 	});
-	textarea.focus();
-	setTimeout(function(){
-		textarea.select();
-		document.execCommand("InsertText", false, new_code);
-		compile(new_code);
+	return new_code;
+}
+
+function getCodeFromPage() {
+	//var textarea = document.querySelector("#code");
+	//var original_code = textarea.value;
+	var cm = document.querySelector('.CodeMirror').CodeMirror;
+	return cm.getValue();
+}
+function setCodeOnPage(new_code) {
+	// TODO for bytebeat: would seem to need to be async
+	//textarea.focus();
+	//setTimeout(function(){
+		//textarea.select();
+		//document.execCommand("InsertText", false, new_code);
+	//});
+	var cm = document.querySelector('.CodeMirror').CodeMirror;
+	cm.setValue(new_code);
+}
+function findCompileFailure() {
+	// TODO: bytebeat
+	return document.querySelector(".tab.errorYes, .CodeMirror .errorMessage");
+}
+function isCompiling() {
+	// TODO: bytebeat
+	return document.querySelector("#compilationTime").textContent.match(/Compiling/i);
+}
+function compileCodeOnPage() {
+	//compile(new_code); // bytebeat
+	document.querySelector("[title~='Compile']").click(); // shadertoy
+
+	return new Promise((resolve, reject)=> {
+		function waitForCompileFinish() {
+			if (isCompiling()) {
+				setTimeout(waitForCompileFinish, 50);
+			} else {
+				var success = !findCompileFailure();
+				if (success) resolve(); else reject(new Error("compile failed"));
+			}
+		}
+		waitForCompileFinish();
 	});
-};
+}
+
+function mutateCodeOnPage() {
+	var original_code = getCodeFromPage();
+	var mutation_chance = 0.05 + 1 / original_code.length;
+
+	var new_code = mutateCode(original_code, mutation_chance);
+	// console.log("new_code", new_code);
+	if (new_code === original_code) {
+		console.log("new_code is same as original_code, LAME");
+	}
+
+	setCodeOnPage(new_code);
+
+	compileCodeOnPage(new_code).then(()=> {
+		console.log("compile succeeded");
+	},	(error)=> {
+		console.log("compile failed");
+	});
+}
+
+function addButtonToUI() {
+	var button = document.getElementById("mutate") || document.createElement("button");
+	button.id = "mutate";
+	button.textContent = "☢ MUTATE ☢";
+	var toolbar = document.querySelector("#toolBar, #toolbar, #tool-bar, #controls");
+	if (location.hostname.match(/ShaderToy/i)) {
+		// let's not bother trying to fit in a layout based around absolute positions
+		// just insert it below the toolbar
+		toolbar.parentElement.insertBefore(button, toolbar.nextSibling);
+	} else {
+		toolbar.appendChild(button);
+	}
+	button.onclick = mutateCodeOnPage;
+}
+
+addButtonToUI();
+mutateCodeOnPage();
+
+/*
+TODO: try sets of edits in a sort of binary search of whether it can compile with a given set (accept set of edits) and whether a particular single edit makes it not compile (reject one edit)
+some other things that would be good:
+support bytebeat again [on windows93.net too]
+khan academy, including "error buddy" detection
+code fiddles like jsfiddle, codepen, jsbin
+maybe insert a header at the top that explains the (wild) modifications (good for forking)
+wrap values sometimes in a function, like i did for:
+	https://www.khanacademy.org/computer-programming/phantasmagoria/2540238893
+	https://www.khanacademy.org/computer-programming/phantasmagoria-plus/6066580222902272
+*/
