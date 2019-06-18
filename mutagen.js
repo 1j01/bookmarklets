583c097e341ea537cc17315755cb943850c934d8
Isaiah Odhner <isaiahodhner@gmail.com>
2019-06-18 13:44:57 -0400

    Make it solid in L shape cases too

--- mutagen.js
+++ mutagen.js
@@ -487,22 +487,12 @@ function draw_logo() {
 		}
 		logo += "\n";
 	}
-	// var basically_anything = [ch_left, ch_right, ch_top, ch_bottom, ch_full, ch_full, "■", "■", "■", "■"];
-	// var diagonal_substitutions = [ch_full, ch_full, "■", "■", "■", "■"];
-	// var diagonal_substitutions = [ch_full];
 	logo = logo
 		.replace(/▘/g, ()=> choose([ch_left, ch_top]))
 		.replace(/▝/g, ()=> choose([ch_right, ch_top]))
 		.replace(/▗/g, ()=> choose([ch_right, ch_bottom]))
 		.replace(/▖/g, ()=> choose([ch_left, ch_bottom]))
-		.replace(/▖/g, ()=> choose([ch_left, ch_bottom]))
-		.replace(/▛/g, ()=> choose([ch_left, ch_top, ch_full]))
-		.replace(/▜/g, ()=> choose([ch_right, ch_top, ch_full]))
-		.replace(/▟/g, ()=> choose([ch_right, ch_bottom, ch_full]))
-		.replace(/▙/g, ()=> choose([ch_left, ch_bottom, ch_full]))
-		// .replace(/▞/g, ()=> choose(diagonal_substitutions))
-		// .replace(/▚/g, ()=> choose(diagonal_substitutions));
-		.replace(/[▚▞]/g, ch_full);
+		.replace(/[▛▜▟▙▚▞]/g, ch_full);
 
 	// add some particles...
 	// this is a silly way of doing this, but hey, it works!
